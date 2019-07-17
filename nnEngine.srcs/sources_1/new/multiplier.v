18b18a87ce8c560995352ead440e5b80d1e790a8
TolgaSari <38347201+TolgaSari@users.noreply.github.com>
2019-07-17 22:01:07 +0300

    Add greedy quantization(4 bit) at multiplier

--- nnEngine.srcs/sources_1/new/multiplier.v
+++ nnEngine.srcs/sources_1/new/multiplier.v
@@ -21,6 +21,8 @@
 // 00.100000 = 0.5       => 0000.100000_000000
 // 11.100000 = - 0.5     => 1111.100000_111111 => 0000.011111_000001
 
+`define GREEDY_QUANTIZATION
+
 module multiplier
 #( 
    parameter FIXED_IN1  = 6,
@@ -35,9 +37,14 @@ module multiplier
     input signed [DATA_WIDTH_IN_2-1:0] num2,
     output signed [DATA_WIDTH_OUT-1:0] result
  );
-    wire signed [DATA_WIDTH_IN_2-1:0] extended_num1 = {{3{num1[6]}}, num1[6:0], {6{1'b0}}};
+    wire signed [DATA_WIDTH_IN_2-1:0] extended_num1;
     wire signed [2*DATA_WIDTH_IN_2-1:0] temp;
     
+    `ifdef GREEDY_QUANTIZATION
+    assign extended_num1 = ({{5{num1[6]}}, num1[6:0], {4{1'b0}}});
+    `else
+    assign extended_num1 = {{3{num1[6]}}, num1[6:0], {6{1'b0}}};
+    `endif
     localparam temp_hi = DATA_WIDTH_OUT + FIXED_OUT - 1;
     localparam temp_low = FIXED_OUT;
     
